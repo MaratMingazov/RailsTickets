2cd481264126de984c4eb3aefe25e77c11681b19
MaratMingazov <maratmingazovr@gmail.com>
2017-05-09 07:26:46 +0300

    Create model for Ad entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506145319) do
+ActiveRecord::Schema.define(version: 20170509041441) do
+
+  create_table "ads", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "seller_id"
+    t.string "email"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
